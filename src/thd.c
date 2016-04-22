91c4218e0022526a26da622b440333cb1605072f
pierreguillot <guillotpierre6@gmail.com>
2016-04-23 01:23:20 +0200

    et ça

--- src/thd.c
+++ src/thd.c
@@ -36,25 +36,24 @@ static DWORD WINAPI internal_method_ptr(LPVOID arg)
     t_internal_parameters *params = (t_internal_parameters *)arg;
     printf("start...");
     ((thd_thread_method_ptr)params->i_method)(params->i_data);
-    free(params);
     printf("end\n");
     return 0;
 }
 
 void thd_thread_launch(thd_thread* thread, thd_thread_method method, void* data)
 {
-    t_internal_parameters* params = (t_internal_parameters *)malloc(sizeof(t_internal_parameters));
-    params->i_method = method;
-    params->i_data   = data;
+    t_internal_parameters params;
+    params.i_method = method;
+    params.i_data   = data;
     if(data)
     {
         printf("call \n");
-        *thread = CreateThread(NULL, 0, internal_method_ptr, params, 0, NULL);
+        *thread = CreateThread(NULL, 0, internal_method_ptr, &params, 0, NULL);
     }
     else
     {
         printf("call \n");
-        *thread = CreateThread(NULL, 0, internal_method_null, params, 0, NULL);
+        *thread = CreateThread(NULL, 0, internal_method_null, &params, 0, NULL);
     }
 }
 
