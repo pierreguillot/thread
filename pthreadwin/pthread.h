0f6fae4b5507597bb8b7fb6ac397f954adbd5b3b
pierreguillot <guillotpierre6@gmail.com>
2016-04-24 15:36:25 +0200

    clean pthread.h

--- pthreadwin/pthread.h
+++ pthreadwin/pthread.h
@@ -13,19 +13,19 @@
 #define pthread_cond_t thd_condition
 #define pthread_t thd_thread
 
-#define pthread_create(t, attr, m, data) thd_thread_detach(t, m, data)
-#define pthread_join(t, attr) (attr); thd_thread_join(&t)
+#define pthread_create(t, attr, m, data)    thd_thread_detach(t, m, data)
+#define pthread_join(t, attr)               thd_thread_join(&t)
 
 #define pthread_mutex_init(mutex, attr) thd_mutex_init(mutex)
-#define pthread_mutex_lock(mutex) thd_mutex_lock(mutex)
-#define pthread_mutex_trylock(mutex) thd_mutex_trylock(mutex)
-#define pthread_mutex_unlock(mutex) thd_mutex_unlock(mutex)
-#define pthread_mutex_destroy(mutex) thd_mutex_destroy(mutex)
+#define pthread_mutex_lock(mutex)       thd_mutex_lock(mutex)
+#define pthread_mutex_trylock(mutex)    thd_mutex_trylock(mutex)
+#define pthread_mutex_unlock(mutex)     thd_mutex_unlock(mutex)
+#define pthread_mutex_destroy(mutex)    thd_mutex_destroy(mutex)
 
-#define pthread_cond_init(cond, attr) thd_condition_init(cond)
-#define pthread_cond_signal(cond) thd_condition_signal(cond)
-#define pthread_cond_wait(cond, mutex) thd_condition_wait(cond, mutex)
-#define pthread_cond_destroy(cond) thd_condition_destroy(cond)
+#define pthread_cond_init(cond, attr)   thd_condition_init(cond)
+#define pthread_cond_signal(cond)       thd_condition_signal(cond)
+#define pthread_cond_wait(cond, mutex)  thd_condition_wait(cond, mutex)
+#define pthread_cond_destroy(cond)      thd_condition_destroy(cond)
 
 #endif // PTHREAD_H
 
